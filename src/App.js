822af00e68d0671a65c89709e265e7ffb93878a2
bekssssss <porshen.2003@mail.ru>
2024-07-30 14:44:39 +0600

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,202 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Img from "./components/assets/react1.png"
+import Img2 from "./components/assets/react2.png"
+import BuyLogo from "./components/assets/buy-logo.png"
+import ErrorLogo from "./components/assets/error-logo.png"
+import ResetLogo from "./components/assets/reset-logo.png"
+import FavLogo from "./components/assets/fav-logo.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="wrapper">
+            <div className="box">
+                <img src={Img} className="first-img"/>
+                <div className="title">
+                    <h1>Always in Style!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores beatae dignissimos ,
+                    </p>
+                    <h4> $219.99</h4>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img2} className="second-img"/>
+                <div className="title-2">
+                    <h1>
+                        129.99 USD
+                    </h1>
+                    <a href="#">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, eum!
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in quam voluptas.
+                    </p>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img} className="first-img"/>
+                <div className="title">
+                    <h1>Always in Style!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores beatae dignissimos ,
+                    </p>
+                    <h4> $219.99</h4>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img2} className="second-img"/>
+                <div className="title-2">
+                    <h1>
+                        129.99 USD
+                    </h1>
+                    <a href="#">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, eum!
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in quam voluptas.
+                    </p>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img2} className="second-img"/>
+                <div className="title-2">
+                    <h1>
+                        129.99 USD
+                    </h1>
+                    <a href="#">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, eum!
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in quam voluptas.
+                    </p>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img} className="first-img"/>
+                <div className="title">
+                    <h1>Always in Style!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores beatae dignissimos ,
+                    </p>
+                    <h4> $219.99</h4>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img2} className="second-img"/>
+                <div className="title-2">
+                    <h1>
+                        129.99 USD
+                    </h1>
+                    <a href="#">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, eum!
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in quam voluptas.
+                    </p>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img2} className="second-img"/>
+                <div className="title-2">
+                    <h1>
+                        129.99 USD
+                    </h1>
+                    <a href="#">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, eum!
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in quam voluptas.
+                    </p>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img2} className="second-img"/>
+                <div className="title-2">
+                    <h1>
+                        129.99 USD
+                    </h1>
+                    <a href="#">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, eum!
+                    </a>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur in quam voluptas.
+                    </p>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img} className="first-img"/>
+                <div className="title">
+                    <h1>Always in Style!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores beatae dignissimos ,
+                    </p>
+                    <h4> $219.99</h4>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img} className="first-img"/>
+                <div className="title">
+                    <h1>Always in Style!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores beatae dignissimos ,
+                    </p>
+                    <h4> $219.99</h4>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+            <div className="box">
+                <img src={Img} className="first-img"/>
+                <div className="title">
+                    <h1>Always in Style!</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores beatae dignissimos ,
+                    </p>
+                    <h4> $219.99</h4>
+                    <img src={BuyLogo} className="logo"/>
+                    <img src={FavLogo} className="logo"/>
+                    <img src={ErrorLogo} className="logo"/>
+                    <img src={ResetLogo} className="logo"/>
+                </div>
+            </div>
+        </div>
+
+    )
 }
-
 export default App;
---
+++ src/components/button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <button className={'btn'}>Click Me!</button>
+    )
+}
+    export default Button;
